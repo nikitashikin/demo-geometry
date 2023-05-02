016c33f7b94290a3239d484db724fdb0417ba367
Mikita Shykin <mikita.shikin@gmail.com>
2023-05-02 17:54:12 +0200

    refactor boxdemo class add one more box object

--- src/BoxDemo.java
+++ src/BoxDemo.java
@@ -1,13 +1,24 @@
 public class BoxDemo {
     public static void main(String[] args) {
-        Box myBox = new Box();
+        Box myBox1 = new Box();
+        Box myBox2 = new Box();
         double vol;
 
-        myBox.width = 10;
-        myBox.height = 20;
-        myBox.depth = 15;
+        myBox1.width = 10;
+        myBox1.height = 20;
+        myBox1.depth = 15;
 
-        vol = myBox.width * myBox.height * myBox.depth;
+        myBox2.width = 3;
+        myBox2.height = 6;
+        myBox2.depth = 9;
+
+
+        vol = myBox2.width * myBox2.height * myBox2.depth;
         System.out.println("Ob'em raven " + vol);
+
+        vol = myBox1.width * myBox1.height * myBox1.depth;
+        System.out.println("Ob'em raven " + vol);
+
+
     }
 }
