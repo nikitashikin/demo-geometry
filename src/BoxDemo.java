d6beb552a002c179628c715260a038c959905d9e
Mikita Shykin <mikita.shikin@gmail.com>
2023-05-02 18:32:57 +0200

    refactor boxdemo class use constructor with params

--- src/BoxDemo.java
+++ src/BoxDemo.java
@@ -1,7 +1,7 @@
 public class BoxDemo {
     public static void main(String[] args) {
-        Box myBox1 = new Box();
-        Box myBox2 = new Box();
+        Box myBox1 = new Box(10,20,15);
+        Box myBox2 = new Box(3,6,9);
         double vol;
 
         vol= myBox1.volume();
